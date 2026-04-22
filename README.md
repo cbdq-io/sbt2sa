789fe271b32c99e0e7d4af8e14a94533466c0afc
Ben Dalling <dallinb@users.noreply.github.com>
2026-04-22 16:14:31 +0100

    feat: minimum viable product

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # sbns2sa
-Extract from an Azure Service Bus Namespace to Blob Storage on a Service Account.
+Extract data from Azure Service Bus Topics and load onto Blob Storage on a Storage Account.
+
+If `CONTAINER_NAME` is set to `mycontainer`, `TOPICS_DIR` is set to `topics`,
+`PATH_FORMAT` is set to `year=YYYY/month=MM/day=dd/hour=HH` and
+`TOPIC_NAME` is set to `mytopic`, then the file path for the data to be
+loaded to will be something like:
+
+`azure://mycontainer/topics/mytopic/year=2025/month=02/day=24/hour=11/mytopic+0000000000000000042.bin.gz`
+
+## Required Environment Variables
+
+- `CONTAINER_NAME`: The container name within the storage account to load
+  the data to.
+- `MAX_MESSAGES_IN_BATCH`: The maximum number of messages to extract from the
+  topic in a batch before loading onto the blob storage.  Default is 500.
+- `SERVICE_BUS_CONNECTION_STRING`:  The connection string to connect to Azure
+  Service Bus.
+- `STORAGE_ACCOUNT_CONNECTION_STRING`: The connection string to connect to
+  the Azure Storage Account.
+- `SUBSCRIPTION_NAME`:  The name of the subscription to use to extract from
+  the topic.
+- `TOPIC_NAME`:  The name of the topic to extract from.
+- `WAIT_TIME_SECONDS`: The length of time in seconds to wait for messages on
+  the topic before continuing.  Default is 5.
+
+## Optional Environment Variables
+
+- `CHECK_FOR_DL_MESSAGES`: Check for the existence of and warn if any dead-
+  letter messages are present on the topic/subscription.  Set to "1" to
+  enable.  Default is "0".
+- `MAX_RUNTIME_SECONDS`: Limits how long (in seconds) the archiver will spend
+  on a single topic before moving on. Set to 0 (default) to disable this and
+  rely on the usual idle detection logic; set to a positive number to enforce
+  a maximum runtime per topic.
+- `PATH_FORMAT`: The configuration to set the format of the data directories.
+  The format set in this configuration converts the timestamp of the latest
+  message in the block written to proper directory strings.  Within the
+  provided string the following substutions will take place:
+    - `YYYY` will be replaced by the year.
+    - `MM` will be replaced by the zero padded month number.
+    - `dd` will be replaced by the zero padded day number.
+    - `HH` will be replaced by the zero padded hour number.
+    - `mm` will be replaced by the zero padded minute number.
+
+  Default is "".
+= `PROMETHEUS_METRIC_NAME_PREFIX`: The prefix for any of the custom
+  Prometheus metrics that are created.  The default is "".
+- `PROMETHEUS_PORT`: Set the port for Prometheus metrics to be made
+  available on when running `multi-topic-entrypoint.py`.  Default
+  is "8000".
+- `TOPICS_DIR`: The directory within the specified container to load the
+  topics to.  Default is `topics`.
---
+++ multi-topic-entrypoint.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+"""A shim for the ghcr.io/cbdq-io/func-sbt-to-blob image to run multiple topics."""
+import os
+import signal
+
+from prometheus_client import Counter, start_http_server
+
+import SBT2Blob
+
+
+class Archivist:
+    """
+    A class for archiving multiple topics and subscriptions.
+
+    Parameters
+    ----------
+    topics_and_subscriptions : str
+        A string of comma separated values that are themselves colon
+        separated values for topic and subscription.
+    """
+
+    def __init__(self, topics_and_subscriptions: str = os.getenv('TOPICS_AND_SUBSCRIPTIONS', '')) -> None:
+        self._topics_and_subscriptions = []
+        self._is_running = True
+        self.topics_and_subscriptions(topics_and_subscriptions)
+        signal.signal(signal.SIGINT, self.stop)
+        signal.signal(signal.SIGTERM, self.stop)
+        prom_metric_prefix = os.getenv('PROMETHEUS_METRIC_NAME_PREFIX', '')
+        self.prom_files_couner = Counter(
+            f'{prom_metric_prefix}file_count',
+            'The number of files processed.'
+        )
+        self.prom_messages_counter = Counter(
+            f'{prom_metric_prefix}message_count',
+            'The number of messages processed.'
+        )
+        self.prometheus_port = int(
+            os.getenv(
+               'PROMETHEUS_PORT',
+               '8000'
+            )
+        )
+
+    def run(self) -> None:
+        """Run the class until a signal is received."""
+        start_http_server(self.prometheus_port)
+
+        while self._is_running:
+            for (topic_name, subscription_name) in self.topics_and_subscriptions():
+                os.environ['TOPIC_NAME'] = topic_name
+                os.environ['SUBSCRIPTION_NAME'] = subscription_name
+                message_count = SBT2Blob.main_wrapper('sbt2blob')
+
+                if message_count:
+                    self.prom_files_couner.inc()
+
+                self.prom_messages_counter.inc(message_count)
+
+    def stop(self, signum, frame) -> None:
+        """Handle a signal if received to initiate a stop."""
+        self._is_running = False
+
+    def topics_and_subscriptions(self, topics_and_subscriptions: str = None) -> list[tuple]:
+        """
+        Get or set the topics and subscriptions.
+
+        Parameters
+        ----------
+        topics_and_subscriptions : str, optional
+            A string of comma separated values that are themselves colon
+            separated values for topic and subscription, by default None
+
+        Returns
+        -------
+        list[tuple]
+            A list of tuples where the fist element is the topic name and the
+            second element is the name of the subscription.
+        """
+        if topics_and_subscriptions is not None:
+            self._topics_and_subscriptions = []
+            items = topics_and_subscriptions.split(',')
+
+            for item in items:
+                self._topics_and_subscriptions.append(tuple(item.split(':')))
+
+        return self._topics_and_subscriptions
+
+
+if __name__ == '__main__':
+    widget = Archivist()
+    widget.run()
